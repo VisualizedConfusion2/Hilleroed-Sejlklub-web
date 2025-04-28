e39c09f5e16ade9fe103016d9f0bd27c706d9f6a
FL03NG <sih013@edu.zealand.dk>
2025-04-28 12:55:23 +0200

    Added Pages

---
+++ Hilleroed Sejlklub web/Pages/EditBoat.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hilleroed_Sejlklub_web.Pages
+{
+    public class EditBoatModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Hilleroed Sejlklub web/Pages/EditBooking.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hilleroed_Sejlklub_web.Pages
+{
+    public class EditBookingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
