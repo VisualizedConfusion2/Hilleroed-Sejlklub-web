8b126e4d5d53302119902ea3c0a8e5aaa5bcc891
VisualizedConfusion2 <thh005.edu.zealand.dk>
2025-04-29 14:43:18 +0200

    changes in interface

--- Hilleroed Sejlklub web/Pages/BoatGrid.cshtml.cs
+++ Hilleroed Sejlklub web/Pages/BoatGrid.cshtml.cs
@@ -1,5 +1,7 @@
+using Hilleroed_Sejlklub_Library;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Drawing.Text;
 
 namespace Hilleroed_Sejlklub_web.Pages
 {
@@ -7,6 +9,13 @@ namespace Hilleroed_Sejlklub_web.Pages
     {
         public void OnGet()
         {
+            private readonly BoatService _bs;
+        [BindProperty]
+        public List<Boat> Boats {get;set;}
+        public BoatGridModel(BoatService bs)
+        {
+            Boats = bs.GetAll();
+        }
         }
     }
 }
